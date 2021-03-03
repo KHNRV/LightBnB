96500f3f3ac1115b2a367129f1e87b92e68fd8e6
Kevin Nicolas <bonjour@kevinnicolas.com>
2021-03-03 14:35:02 +0000

    Add 1_queries

---
+++ 1_queries/5_all_my_reservations.sql
@@ -0,0 +1,9 @@
+SELECT reservations.*, properties.*, avg(rating) as average_rating 
+FROM properties
+JOIN reservations ON properties.id = reservations.property_id
+JOIN property_reviews ON properties.id = property_reviews.property_id
+WHERE reservations.guest_id = 1
+AND end_date < now()::date
+GROUP BY properties.id, reservations.id
+ORDER BY start_date ASC 
+LIMIT 10;
